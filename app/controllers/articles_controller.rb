64598579b8dae306f7e8d360712c92f75f911d4f
Jo Forchione <joforchione@jos-macbook-air.attlocal.net>
2016-05-19 17:45:57 -0700

    finished blog comments feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,6 +8,8 @@ class ArticlesController < ApplicationController
 
   def show
     @article = Article.find(params[:id])
+    @comment = Comment.new
+    @comment.article_id = @article.id
   end
 
   def new
@@ -18,6 +20,8 @@ class ArticlesController < ApplicationController
     @article = Article.new(article_params)
     @article.save
 
+    flash.notice = "Article '#{@article.title}' Created!"
+
     redirect_to article_path(@article)
   end
 
@@ -25,10 +29,21 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.destroy
 
+    flash.notice = "Article '#{@article.title}' Deleted!"
+
     redirect_to articles_path(@index)
   end
 
   def edit
     @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to article_path(@article)
   end
 end
